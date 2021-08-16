677cc1dae738f1219fea4ce21b0efccbb73ac1d1
fcfc2021 <87288821+fcfc2021@users.noreply.github.com>
2021-08-16 12:40:08 +0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,14 @@ module.exports = {
   "env": {
     "browser": true,
     "es6": true,
-    "jest/globals": true
+    "jest/globals": true,
+    "cypress/globals": true
   },
   "extends": [
     "eslint:recommended",
-    "plugin:react/recommended"
+    "plugin:jest/recommended",
+    "plugin:react/recommended",
+    "plugin:cypress/recommended"
   ],
   "parserOptions": {
     "ecmaFeatures": {
@@ -16,7 +19,7 @@ module.exports = {
     "sourceType": "module"
   },
   "plugins": [
-    "react", "jest"
+    "react", "jest", "cypress"
   ],
   "rules": {
     "indent": [
