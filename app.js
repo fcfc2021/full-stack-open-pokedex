7f112e6e58892c1a5414f84d97892d324884564d
fcfc2021 <87288821+fcfc2021@users.noreply.github.com>
2021-08-17 09:37:42 +0800

    added health check endpoint

--- app.js
+++ app.js
@@ -6,6 +6,17 @@ const PORT = process.env.PORT || 5000 // eslint-disable-line no-undef
 
 app.use(express.static('dist'))
 
+//health check copied from part11c
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+
+app.get('/version', (req, res) => {
+  res.send('1.0.0') // change this string to ensure a new version deployed
+})
+
+
 app.listen(PORT, () => {
 
   console.log('server started on port 5000')   // eslint-disable-line no-console
